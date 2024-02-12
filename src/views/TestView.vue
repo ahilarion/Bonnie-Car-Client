a82e88b4f49d3b95873cd571bcf36c4fca58f22e
ThorKild_ <pro.antoine.masia@gmail.com>
2024-02-12 10:34:08 +0100

    correction taille post

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,12 +1,11 @@
 <script setup>
-
 import Post from "@/components/Post.vue";
 import Sponsors from "@/components/Sponsors.vue";
 import LoginForm from "@/components/LoginForm.vue";
 </script>
 
 <template>
-    <!-- <Sponsors /> -->
-    <!-- <Post /> -->
-    <LoginForm />
+  <!-- <Sponsors /> -->
+  <Post />
+  <!-- <LoginForm /> -->
 </template>
