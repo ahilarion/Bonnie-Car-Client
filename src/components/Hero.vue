a72c65f81cd18425adb2e82a484ecc1f09107a8d
ThorKild_ <pro.antoine.masia@gmail.com>
2024-02-12 10:15:55 +0100

    tuto pour tristan

---
+++ src/components/Hero.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="my-16">
+    <h1 class="text-bc-white">{{ title }}</h1>
+    <slot></slot>
+  </div>
+</template>
---
+++ src/components/InputCustom.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    required: true,
+  },
+  placeholder: {
+    type: String,
+    default: "test",
+  },
+});
+</script>
+
+<template>
+  <input
+    class="h-12 rounded-xl bg-[transparent] w-full border-none text-bc-yellow"
+    :type="type"
+    :placeholder="placeholder"
+  />
+</template>
